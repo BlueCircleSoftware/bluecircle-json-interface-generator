8179346c781ed4b0291028ac6254ab82bf0a920a
Marc Ramirez <mrami@bluecirclesoft.com>
2017-06-28 03:37:44 -0400

    Add integration test for object consume/produce

--- json-integration-tests/src/test/java/com/bluecirclesoft/open/jigen/integration/JEEToTypeScriptTest.java
+++ json-integration-tests/src/test/java/com/bluecirclesoft/open/jigen/integration/JEEToTypeScriptTest.java
@@ -51,7 +51,7 @@ public class JEEToTypeScriptTest {
 		try {
 			log.info("Creating deployment");
 			WebArchive jar = ShrinkWrap.create(WebArchive.class)
-					.addClasses(TestServicesString.class, JaxRsActivator.class)
+					.addPackage("com.bluecirclesoft.open.jigen.integration")
 					.addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
 			log.info("Created deployment: {}", jar.toString(true));
 			return jar;
