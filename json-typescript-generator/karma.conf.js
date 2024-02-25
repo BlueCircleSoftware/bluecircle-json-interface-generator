08ce939f88e7f8f8848addf85705ec4516022784
Marc Ramirez <marc.ramirez@bluecirclesoft.com>
2024-02-25 15:00:40 -0500

    Can we re-enable Firefox?

--- json-typescript-generator/karma.conf.js
+++ json-typescript-generator/karma.conf.js
@@ -59,7 +59,7 @@ module.exports = function (config) {
 
         // start these browsers
         // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-        browsers: [/*'Firefox',*/ 'ChromeNoSandbox'],
+        browsers: ['Firefox', 'ChromeNoSandbox'],
 
         // need to do this in Jenkins, because it's a Docker image
         customLaunchers: {
