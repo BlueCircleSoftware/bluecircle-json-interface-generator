4c4c295dfc27fda9e07a950b9798e1b816d5edff
Marc Ramirez <marc.ramirez@bluecirclesoft.com>
2025-09-03 01:12:58 -0400

    Fix Sonatype requirements

---
+++ json-generator-allin1/src/main/java/com/bluecirclesoft/open/jsongenerator/allin1/package-info.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2025 Blue Circle Software, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+/**
+ * The all-in-1 artifact includes all the different readers and generators, for use in environments where you want to
+ * use a single library.
+ */
+package com.bluecirclesoft.open.jsongenerator.allin1;
